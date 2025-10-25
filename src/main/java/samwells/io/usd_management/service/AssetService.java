958c30e0ba46404d48d3ddc5767c7ef4e05b782a
Sam Wells <samwells95@gmail.com>
2025-10-24 22:17:34 -0700

    Testing basic asset retrieval

--- src/main/java/samwells/io/usd_management/service/AssetService.java
+++ src/main/java/samwells/io/usd_management/service/AssetService.java
@@ -1,16 +1,26 @@
 package samwells.io.usd_management.service;
 
+import com.google.protobuf.ByteString;
 import io.grpc.stub.StreamObserver;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.grpc.server.service.GrpcService;
 import studio.assets.AssetServiceGrpc;
 import studio.assets.Assets;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 import java.util.UUID;
 
+@Slf4j
 @GrpcService
 // AssetServiceGrpc.AssetServiceImplBase is generated based on the .proto files
 public class AssetService extends AssetServiceGrpc.AssetServiceImplBase {
+    private final byte[] testFile;
+    public AssetService() {
+        this.testFile = readResourceFile("big_ass_cube.usd");
+    }
+
     @Override
     public void listAssets(Assets.ListAssetsRequest request, StreamObserver<Assets.ListAssetsResponse> responseObserver) {
 
@@ -35,5 +45,31 @@ public class AssetService extends AssetServiceGrpc.AssetServiceImplBase {
 
         responseObserver.onNext(response);
         responseObserver.onCompleted();
+    }
+
+    @Override
+    public void getAsset(Assets.GetAssetRequest request, StreamObserver<Assets.GetAssetResponse> responseObserver) {
+        log.info("Got request for asset {}", request.getId());
+
+        Assets.GetAssetResponse response = Assets
+                .GetAssetResponse
+                .newBuilder()
+                .setFormat("USD")
+                .setData(ByteString.copyFrom(testFile))
+                .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+
+    private byte[] readResourceFile(String fileName) {
+        try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream(fileName)) {
+            if (inputStream == null) throw new IllegalArgumentException("Resource not found: " + fileName);
+
+            return inputStream.readAllBytes();
+        } catch (IOException e) {
+            log.error("Failed to read resource: {}", fileName, e);
+            throw new RuntimeException(e);
+        }
     }
 }
